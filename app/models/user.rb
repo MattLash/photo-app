21809d58403eec029b054132c8b5e5e50950ae6f
Matt Lashinsky <malashinsky@gmail.com>
2016-02-23 15:32:17 +0000

    added amazon bucket

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,3 +8,6 @@ class User < ActiveRecord::Base
   accepts_nested_attributes_for :payment
   has_many :images
 end
+
+# Access Key ID:AKIAJWOCRCB2W6GW54SQ
+# Secret Access Key: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
